21385e19e29c5d47a03fdc7205bac3641e4f9191
Alexa Hines <99751726+a-hines@users.noreply.github.com>
2022-10-24 19:39:27 -0400

    Previous comment should have said Search instead of Reviews

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ElectricBike from "../assets/images"
 
+// adding comment to push to Github again
 const Search = ()=>{
     return(
       <Card style={{width: '18rem'}}>
