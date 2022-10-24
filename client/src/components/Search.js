74b75fadba8f8cd47c6ae6b3647f1b73a8429ed0
Alexa Hines <99751726+a-hines@users.noreply.github.com>
2022-10-24 19:36:57 -0400

    Reviews initial set up

---
+++ client/src/components/Search.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ElectricBike from "../assets/images"
+
+const Search = ()=>{
+    return(
+      <Card style={{width: '18rem'}}>
+        <Card.Img variant="top" src={ElectricBike} />
+        <Card.Body>
+            <Card.Title>Product Name</Card.Title>
+            <button>Rent Now!</button>
+        </Card.Body>
+
+      </Card>
+    )
+}
+
+export default Search;
