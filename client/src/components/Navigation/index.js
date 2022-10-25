62071e8ef5e0f6d1bebe9e6117a75019faff3bc0
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-10-25 12:04:18 -0400

    created login/signup modal

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -1,63 +1,119 @@
-import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { Navbar, Nav, Container, Modal, Tab } from "react-bootstrap";
+import SignUpForm from "../SignupForm";
+import LoginForm from "../LoginForm";
+
+import Auth from "../../utils/auth";
 
 const Navigation = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <Navbar expand="lg" fixed="top">
-      <Container>
-        <Navbar.Brand className="brand">
-          <NavLink style={{ textDecoration: "none", color: "black" }} to="/">
-            NO<span style={{ color: "#558C8F" }}>FOMO</span>
-          </NavLink>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto mx-5">
-            <NavLink
-              style={({ isActive }) => ({
-                color: isActive ? "#558C8F" : "black",
-                textDecoration: "none",
-              })}
-              to="home"
-              className="nav"
-            >
-              Home
+    <>
+      <Navbar expand="lg" fixed="top">
+        <Container>
+          <Navbar.Brand className="brand">
+            <NavLink style={{ textDecoration: "none", color: "black" }} to="/">
+              NO<span style={{ color: "#558C8F" }}>FOMO</span>
             </NavLink>
-            <NavLink
-              style={({ isActive }) => ({
-                color: isActive ? "#558C8F" : "black",
-                textDecoration: "none",
-              })}
-              to="categories"
-              className="nav"
-            >
-              Categories
-            </NavLink>
-            <NavLink
-              style={({ isActive }) => ({
-                color: isActive ? "#558C8F" : "black",
-                textDecoration: "none",
-              })}
-              to="reviews"
-              className="nav cta"
-            >
-              Reviews
-            </NavLink>
-            <NavLink
-              style={({ isActive }) => ({
-                color: isActive ? "#558C8F" : "black",
-                textDecoration: "none",
-              })}
-              to="signup"
-              className="nav cta"
-            >
-              Sign up | Login
-            </NavLink>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto mx-5">
+              <NavLink
+                style={({ isActive }) => ({
+                  color: isActive ? "#558C8F" : "black",
+                  textDecoration: "none",
+                })}
+                to="home"
+                className="nav"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                style={({ isActive }) => ({
+                  color: isActive ? "#558C8F" : "black",
+                  textDecoration: "none",
+                })}
+                to="categories"
+                className="nav"
+              >
+                Categories
+              </NavLink>
+              <NavLink
+                style={({ isActive }) => ({
+                  color: isActive ? "#558C8F" : "black",
+                  textDecoration: "none",
+                })}
+                to="reviews"
+                className="nav"
+              >
+                Reviews
+              </NavLink>
+              {Auth.loggedIn() ? (
+                <>
+                  <NavLink className="nav signup" onClick={Auth.logout}>
+                    Logout
+                  </NavLink>
+                </>
+              ) : (
+                <NavLink
+                  className="nav signup"
+                  onClick={() => setShowModal(true)}
+                >
+                  Login/Sign Up
+                </NavLink>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Modal
+        size="lg"
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        aria-labelledby="signup-modal"
+      >
+        {/* tab container to do either signup or login component */}
+        <Tab.Container defaultActiveKey="login">
+          <Modal.Header closeButton>
+            <Modal.Title id="signup-modal">
+              <Nav variant="pills">
+                <Nav.Item>
+                  <Nav.Link
+                    eventKey="login"
+                    style={{ backgroundColor: "#558C8F", marginRight: 10 }}
+                    active={{ color: "#558C8F", backgroundColor: "white" }}
+                  >
+                    Login
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link
+                    eventKey="signup"
+                    style={{ backgroundColor: "#558C8F" }}
+                    active={{ color: "#558C8F", backgroundColor: "white" }}
+                  >
+                    Sign Up
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Tab.Content>
+              <Tab.Pane eventKey="login">
+                <LoginForm handleModalClose={() => setShowModal(false)} />
+              </Tab.Pane>
+              <Tab.Pane eventKey="signup">
+                <SignUpForm handleModalClose={() => setShowModal(false)} />
+              </Tab.Pane>
+            </Tab.Content>
+          </Modal.Body>
+        </Tab.Container>
+      </Modal>
+    </>
   );
 };
 
