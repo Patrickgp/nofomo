bb4f86ba122b4a8ed005b3ebb144c6947464812e
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-10-25 23:07:51 -0400

    edited carousel component for better scalability from mobile to desktop

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,145 +11,148 @@ import Tent from "../assets/tent.webp";
 
 const Home = () => {
   return (
-    <Carousel className="bg-dark featuredItem">
-      <Carousel.Item>
-        <Stack
-          direction="horizontal"
-          className="justify-content-center align-items-center"
-          gap={4}
-        >
-          <Card className="bg-light mt-5" style={{ width: 300 }}>
-            <Card.Img src={Tent} alt="Card image" />
-            <Card.ImgOverlay>
-              <Card.Text>
-                <Button
-                  style={{ position: "absolute", right: 5, bottom: 5 }}
-                  variant="primary"
-                >
-                  Rent Now
-                </Button>
-              </Card.Text>
-            </Card.ImgOverlay>
-            <Card.Body>
-              <Card.Title>{}</Card.Title>
-              <Card.Text>{}</Card.Text>
-            </Card.Body>
-          </Card>
-
-          <Card className="bg-light mt-5" style={{ width: 300 }}>
-            <Card.Img src={ElectricBike} alt="Card image" />
-            <Card.ImgOverlay>
-              <Card.Text>
-                <Button
-                  style={{ position: "absolute", right: 5, bottom: 5 }}
-                  variant="primary"
-                >
-                  Rent Now
-                </Button>
-              </Card.Text>
-            </Card.ImgOverlay>
-            <Card.Body>
-              <Card.Title>{}</Card.Title>
-              <Card.Text>{}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Stack>
-      </Carousel.Item>
-
-      <Carousel.Item>
-        <Stack
-          direction="horizontal"
-          className="justify-content-center align-items-center"
-          gap={4}
-        >
-          <Card
-            className="bg-light mt-5 featuredItemCard"
-            style={{ width: 300 }}
+    <>
+      <h3 className="featureHeader">Featured Items</h3>
+      <Carousel className="bg-dark featuredItem">
+        <Carousel.Item>
+          <Stack
+            direction="horizontal"
+            className="justify-content-center align-items-center"
+            gap={4}
           >
-            <Card.Img src={Kayak} alt="Card image" />
-            <Card.ImgOverlay>
-              <Card.Text>
-                <Button
-                  style={{ position: "absolute", right: 5, bottom: 5 }}
-                  variant="primary"
-                >
-                  Rent Now
-                </Button>
-              </Card.Text>
-            </Card.ImgOverlay>
-            <Card.Body>
-              <Card.Title>{}</Card.Title>
-              <Card.Text>{}</Card.Text>
-            </Card.Body>
-          </Card>
+            <Card className="bg-light mt-5" style={{ width: 300 }}>
+              <Card.Img src={Tent} alt="Card image" />
+              <Card.ImgOverlay>
+                <Card.Text>
+                  <Button
+                    style={{ position: "absolute", right: 5, bottom: 5 }}
+                    variant="primary"
+                  >
+                    Rent Now
+                  </Button>
+                </Card.Text>
+              </Card.ImgOverlay>
+              <Card.Body>
+                <Card.Title>{}</Card.Title>
+                <Card.Text>{}</Card.Text>
+              </Card.Body>
+            </Card>
 
-          <Card className="bg-light mt-5" style={{ width: 300 }}>
-            <Card.Img src={Mixer} alt="Card image" />
-            <Card.ImgOverlay>
-              <Card.Text>
-                <Button
-                  style={{ position: "absolute", right: 5, bottom: 5 }}
-                  variant="primary"
-                >
-                  Rent Now
-                </Button>
-              </Card.Text>
-            </Card.ImgOverlay>
-            <Card.Body>
-              <Card.Title>{}</Card.Title>
-              <Card.Text>{}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Stack>
-      </Carousel.Item>
+            <Card className="bg-light mt-5" style={{ width: 300 }}>
+              <Card.Img src={ElectricBike} alt="Card image" />
+              <Card.ImgOverlay>
+                <Card.Text>
+                  <Button
+                    style={{ position: "absolute", right: 5, bottom: 5 }}
+                    variant="primary"
+                  >
+                    Rent Now
+                  </Button>
+                </Card.Text>
+              </Card.ImgOverlay>
+              <Card.Body>
+                <Card.Title>{}</Card.Title>
+                <Card.Text>{}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Stack>
+        </Carousel.Item>
 
-      <Carousel.Item>
-        <Stack
-          direction="horizontal"
-          className="justify-content-center align-items-center"
-          gap={4}
-        >
-          <Card
-            className="bg-light mt-5 featuredItemCard"
-            style={{ width: 300 }}
+        <Carousel.Item>
+          <Stack
+            direction="horizontal"
+            className="justify-content-center align-items-center"
+            gap={4}
           >
-            <Card.Img src={PatioHeater} alt="Card image" />
-            <Card.ImgOverlay>
-              <Card.Text>
-                <Button
-                  style={{ position: "absolute", right: 5, bottom: 5 }}
-                  variant="primary"
-                >
-                  Rent Now
-                </Button>
-              </Card.Text>
-            </Card.ImgOverlay>
-            <Card.Body>
-              <Card.Title>{}</Card.Title>
-              <Card.Text>{}</Card.Text>
-            </Card.Body>
-          </Card>
+            <Card
+              className="bg-light mt-5 featuredItemCard"
+              style={{ width: 300 }}
+            >
+              <Card.Img src={Kayak} alt="Card image" />
+              <Card.ImgOverlay>
+                <Card.Text>
+                  <Button
+                    style={{ position: "absolute", right: 5, bottom: 5 }}
+                    variant="primary"
+                  >
+                    Rent Now
+                  </Button>
+                </Card.Text>
+              </Card.ImgOverlay>
+              <Card.Body>
+                <Card.Title>{}</Card.Title>
+                <Card.Text>{}</Card.Text>
+              </Card.Body>
+            </Card>
 
-          <Card className="bg-light mt-5" style={{ width: 300 }}>
-            <Card.Img src={TableSaw} alt="Card image" />
-            <Card.ImgOverlay>
-              <Card.Text>
-                <Button
-                  style={{ position: "absolute", right: 5, bottom: 5 }}
-                  variant="primary"
-                >
-                  Rent Now
-                </Button>
-              </Card.Text>
-            </Card.ImgOverlay>
-            <Card.Body>
-              <Card.Title>{}</Card.Title>
-              <Card.Text>{}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Stack>
-      </Carousel.Item>
-    </Carousel>
+            <Card className="bg-light mt-5" style={{ width: 300 }}>
+              <Card.Img src={Mixer} alt="Card image" />
+              <Card.ImgOverlay>
+                <Card.Text>
+                  <Button
+                    style={{ position: "absolute", right: 5, bottom: 5 }}
+                    variant="primary"
+                  >
+                    Rent Now
+                  </Button>
+                </Card.Text>
+              </Card.ImgOverlay>
+              <Card.Body>
+                <Card.Title>{}</Card.Title>
+                <Card.Text>{}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Stack>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <Stack
+            direction="horizontal"
+            className="justify-content-center align-items-center"
+            gap={4}
+          >
+            <Card
+              className="bg-light mt-5 featuredItemCard"
+              style={{ width: 300 }}
+            >
+              <Card.Img src={PatioHeater} alt="Card image" />
+              <Card.ImgOverlay>
+                <Card.Text>
+                  <Button
+                    style={{ position: "absolute", right: 5, bottom: 5 }}
+                    variant="primary"
+                  >
+                    Rent Now
+                  </Button>
+                </Card.Text>
+              </Card.ImgOverlay>
+              <Card.Body>
+                <Card.Title>{}</Card.Title>
+                <Card.Text>{}</Card.Text>
+              </Card.Body>
+            </Card>
+
+            <Card className="bg-light mt-5" style={{ width: 300 }}>
+              <Card.Img src={TableSaw} alt="Card image" />
+              <Card.ImgOverlay>
+                <Card.Text>
+                  <Button
+                    style={{ position: "absolute", right: 5, bottom: 5 }}
+                    variant="primary"
+                  >
+                    Rent Now
+                  </Button>
+                </Card.Text>
+              </Card.ImgOverlay>
+              <Card.Body>
+                <Card.Title>{}</Card.Title>
+                <Card.Text>{}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Stack>
+        </Carousel.Item>
+      </Carousel>
+    </>
   );
 };
 
