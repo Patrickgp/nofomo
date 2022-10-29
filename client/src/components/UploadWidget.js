01b6c68e406520b0b7de524f13c17546987d7d7d
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-10-29 08:49:22 -0400

    testing ways to save publicId to photo to conditionally render that image back into user post

--- client/src/components/UploadWidget.js
+++ client/src/components/UploadWidget.js
@@ -1,21 +1,29 @@
 import { useEffect, useRef } from "react";
 
-const UploadWidget = () => {
+import { useQuery } from "@apollo/client";
+import { QUERY_ME } from "../utils/queries";
+
+function UploadWidget() {
   const cloudinaryRef = useRef();
   const widgetRef = useRef();
+
+  const { data } = useQuery(QUERY_ME);
+  const userdata = data?.me || {};
+
   useEffect(() => {
     cloudinaryRef.current = window.cloudinary;
     widgetRef.current = cloudinaryRef.current.createUploadWidget(
       {
-        cloudName: "dk05bpck6",
-        uploadPreset: "ppdspvfz",
+        cloudName: "dkqszfipi",
+        uploadPreset: "mj0ylsvt",
+        publicId: `${userdata.username}${userdata.listingId}`,
       },
       function (err, result) {
         console.log(result);
       }
     );
-  }, []);
+  }, [userdata.username, userdata.listingId]);
   return <button onClick={() => widgetRef.current.open()}>Upload</button>;
-};
+}
 // https://www.youtube.com/watch?v=paiO6M2wBqE&ab_channel=Cloudinary
 export default UploadWidget;
