6b3ebfd6b1d2cd6ca01267892163b71c3da9d739
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-10-30 22:03:58 -0400

    cors already present

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const productsRoute = require("./routes/product");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
-const cors = require("cors");
 
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
