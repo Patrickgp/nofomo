6d17bd7ce37f1c553c8e0596cedc61078c429796
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-10-31 21:09:52 -0400

    updated express to handle 50mb limit in server.js

--- server/server.js
+++ server/server.js
@@ -20,7 +20,11 @@ const server = new ApolloServer({
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(
+  express.json({
+    limit: "50mb",
+  })
+);
 app.use(cors());
 
 app.use("/api/products", productsRoute);
