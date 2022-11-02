7dcb98faee8e0643cafc0ad022fbbd503c9ebd12
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-11-01 20:11:42 -0400

    bug fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/nofomo',
+  process.env.MONGODB_URI,
+  // || 'mongodb://127.0.0.1:27017/nofomo',
   {
-    useNewUrlParser: true, 
+    useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
