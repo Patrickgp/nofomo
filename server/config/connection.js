973c55c1c60c2d442b3435a071d055a06b9ba942
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-11-01 21:01:27 -0400

    fixing deploy bugs

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb+srv://nighthawk:nofomo@nofomo.76lpu4l.mongodb.net/nofomo?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = mongoose.connection;
