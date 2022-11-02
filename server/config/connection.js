843f09e09c4085f9106b266ff79ead9530c4bcce
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-11-02 00:01:57 -0400

    bug fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,13 +3,20 @@ const mongoose = require("mongoose");
 const uri =
   "mongodb+srv://nighthawk:nofomo@nofomo.76lpu4l.mongodb.net/nofomo?retryWrites=true&w=majority";
 
-mongoose.connect(
-  uri,
-  // process.env.MONGODB_URI || "mongodb://localhost:27017/nofomo",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose
+  .connect(
+    uri,
+    // process.env.MONGODB_URI || "mongodb://localhost:27017/nofomo",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then((result) => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(`Error connecting to MongoDB: `, error.message);
+  });
 
 module.exports = mongoose.connection;
