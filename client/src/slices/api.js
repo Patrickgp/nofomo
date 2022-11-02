1f0d1a47287b19b053f5d54efcd1b87eadd5aa9e
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-11-02 01:00:14 -0400

    bug fix

--- client/src/slices/api.js
+++ client/src/slices/api.js
@@ -1,4 +1,6 @@
-export const url = "http://localhost:3001/api";
+// export const url = "http://localhost:3001/api";
+
+export const url = "https://shrouded-eyrie-44498.herokuapp.com/";
 
 export const setHeaders = () => {
   const headers = {
