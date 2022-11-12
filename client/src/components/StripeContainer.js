7509e6e6cd0fb229974e9f4a90ee93d65c687b29
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-11-12 10:53:50 -0500

    reactivated Stripe.js for testing

--- client/src/components/StripeContainer.js
+++ client/src/components/StripeContainer.js
@@ -4,7 +4,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import PaymentForm from "./PaymentForm.js";
 
 const PUBLIC_KEY =
-  "pk_test_51LulVYIYYlWs0IlM5ryYG8XPRJqztmGJBa3c3T0i8G1XbSRGqYvim0mNHWPi2HKmf1ugO0R8wNYNMD9ih8CQuo5000vm2tMn3g";
+  "pk_test_51M3LzhF8ul5vSrSUxAAsbJvGrniCUcnk5TPuYg4G5ynnNs4b4ahdhtrm0soIb1p0KSPYV5kUpyO13vIvKmTNcqfd00zbfu09DE";
 
 const stripeTestPromise = loadStripe(PUBLIC_KEY);
 
